1b6c7027a49611ec10f65ed1c7c8078d8199e8ca
sachin <sachin1112@gmail.com>
2022-09-09 23:33:05 +0530

    ghj

---
+++ src/main/java/com/addingmvnplugin/App.java
@@ -0,0 +1,13 @@
+package com.addingmvnplugin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
